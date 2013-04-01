4ce6734b451cdc76e003d697ed49488f20bd4254
fernando <fernando.ramallo@gmailcom>
2013-03-31 23:48:41 -0700

    * color

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,11 +4,31 @@ using System.Collections;
 public class Game : MonoBehaviour {
 	
 	public Texture2D[] textures;
+	public Texture2D palette;
 	
 	public static Game instance;
 	
+	private Color[] _colors;
+	
 	void Awake() {
 		if (instance == null) instance = this;
+		
+		_colors = new Color[palette.width * palette.height];
+		int i = 0;
+		for (int x = 0; x < palette.width; x++) {
+			for (int y = 0; y < palette.height; y++) {
+				_colors[i] = palette.GetPixel(x,y);
+				i++;
+			}
+		}
+	}
+	
+	void Start() {
+		
+		Color c = getRandomColor();
+		transform.FindChild("camera").camera.backgroundColor = c;
+		transform.FindChild("light").light.color = c;
+		RenderSettings.fogColor = c;
 	}
 	
 	
@@ -16,5 +36,8 @@ public class Game : MonoBehaviour {
 		return textures[Mathf.FloorToInt(Random.value * (textures.Length-1))];
 	}
 	
+	public Color getRandomColor() {
+		return _colors[Mathf.FloorToInt(Random.value * (_colors.Length-1))];
+	}
 	
 }
--- Assets/Scripts/RandomColor.cs
+++ Assets/Scripts/RandomColor.cs
@@ -5,11 +5,6 @@ public class RandomColor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		renderer.material.color = Game.instance.getRandomColor();
 	}
 }
